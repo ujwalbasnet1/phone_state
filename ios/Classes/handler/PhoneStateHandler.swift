08159bc0e16088e4ed5f331923cc497c4ca01c90
ujwalbasnet1 <ujwalbasnet1@gmail.com>
2024-02-07 15:12:22 +0545

    rollback

--- ios/Classes/handler/PhoneStateHandler.swift
+++ ios/Classes/handler/PhoneStateHandler.swift
@@ -34,46 +34,15 @@ class PhoneStateHandler: NSObject, FlutterStreamHandler, CXCallObserverDelegate{
         // Map
         if(_eventSink != nil) { 
             
-            fetchFirstCallFromHistory { firstCall in
-                if let firstCall = firstCall {
-                    let phoneNumber = firstCall.phoneNumber
-                    let callType = firstCall.callType
-                    let date = firstCall.date
-                    print("First call from history:")
-                    print("Phone Number: \(phoneNumber)")
-                    print("Call Type: \(callType)")
-                    print("Date: \(date)")
-
-                     _eventSink!(
-                        [
-                            "status": status.rawValue,
-                            "phoneNumber": phoneNumber
-                        ]
-                    )
-
-                } else {
-                    print("No call history available.")
-                }
-            }
+            _eventSink!(
+               [
+                   "status": status.rawValue,
+                   "phoneNumber": call.uuid.uuidString
+               ]
+           )
         }
     }
 
-    func fetchFirstCallFromHistory(completion: @escaping (CXCallRecord?) -> Void) {
-        let store = CXCallDirectoryProvider.shared
-        store.getCallHistory() { callHistory, error in
-            if let error = error {
-                print("Error fetching call history: \(error.localizedDescription)")
-                completion(nil)
-                return
-            }
-
-            // Sort call history by date
-            let sortedCallHistory = callHistory.sorted { $0.date > $1.date }
-
-            // Return the first item of the sorted call history
-            completion(sortedCallHistory.first)
-        }
-    }
 
 
     
